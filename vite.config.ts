ba41794b0f0eb7e73ddf1b5a54c9da1f75bff418
Arthur <goolebyaqa@gmail.com>
2024-09-09 22:24:58 +0500

    fix: some changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/testdeploy/',
   assetsInclude: ["**/*.jpg", "**/*.png"],
   server: {
     proxy: {
