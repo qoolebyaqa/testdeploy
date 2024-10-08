5ff170a8d3c6a81dc53fc5776e5fb85b62604090
Artur Ten <artur_ten@epam.com>
2024-10-08 21:51:51 +0500

    fix: prevent default behavior of form

--- src/pages/Password.tsx
+++ src/pages/Password.tsx
@@ -1,6 +1,6 @@
 /* import Authentification from "./Authentification"; */
 
-import { /* useEffect, */ useState } from "react";
+import { /* useEffect, */ FormEvent, useState } from "react";
 /* import CustomInput from "../components/UI/CustomInput"; */
 import { useLoaderData } from "react-router";
 import { ApiService } from "../helpers/API/ApiSerivce";
@@ -13,7 +13,8 @@ function Password() {
 const [phone, setPhone] = useState(''); 
 const params:any = useLoaderData();
 
-async function handleSubmit() {
+async function handleSubmit(e:FormEvent) {
+  e.preventDefault();
   try {
     await ApiService.getOTP(params.tkn, phone)
   } catch (err) {
@@ -22,13 +23,15 @@ async function handleSubmit() {
 }
 
 return ( 
-    
+    <div className="bg-black w-screen h-screen flex items-center justify-center">
     <form onSubmit={handleSubmit}>
+      <p className="font-extrabold text-xl w-[300px] text-center mb-4">Введите номер телефона для отправки кода</p>
       <label htmlFor="phone">
       <input type="phone" placeholder="Введите номер телефона" onChange={(e) => setPhone(e.target.value)}/>
       </label>
-      <button>Отправить КОД</button>
+      <button disabled={!phone}>Отправить КОД</button>
     </form>
+    </div>
    );
 }
 
